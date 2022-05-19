4021a1669ea90540b335f502d357fbc4433f1016
furkanogutcu <furkan.ogutcu@bil.omu.edu.tr>
2022-05-19 17:47:16 +0300

    client-Store'u duzelt
    Bu taahhut Store'daki User objesini Subscriber olarak gunceller.
    
    Ayrıca bu Subscriber objesinin server'dan alinmasini saglar.

--- client/store/index.js
+++ client/store/index.js
@@ -1,48 +1,66 @@
-import jwtDecode from 'jwt-decode';
-
 export const MUTATIONS = {
-    SET_USER: 'SET_USER',
-    DELETE_USER: 'DELETE_USER',
+    SET_SUBSCRIBER: 'SET_SUBSCRIBER',
+    DELETE_SUBSCRIBER: 'DELETE_SUBSCRIBER',
     CHANGE_PAGE: 'CHANGE_PAGE',
 };
 
 export const state = () => ({
-    user: null,
+    subscriber: {
+        _id: '',
+        firstName: '',
+        lastName: '',
+        email: '',
+        roles: [],
+        subscribedCategories: [],
+        createdAt: '',
+        updatedAt: '',
+        analysis: {
+            totalCampaignClicks: 0,
+            totalNumberOfEmailSent: 0
+        }
+    },
     activePage: 1,
 });
 
 export const getters = {
-    user: state => state.user,
+    getSubscriber: state => state.subscriber,
     activePage: state => state.activePage,
 };
 
 export const mutations = {
-    [MUTATIONS.SET_USER](state) {
-        const tokenWithBearer = this.$auth.strategy.token.get();
-        if (tokenWithBearer === false) {
-            this.$auth.logout();
-            state.user = null;
-            return;
-        }
-        const decoded = jwtDecode(tokenWithBearer.split(' ')[1]);
-        state.user = decoded.user;
+    [MUTATIONS.SET_SUBSCRIBER](state, subscriber) {
+        state.subscriber = subscriber;
     },
-    [MUTATIONS.DELETE_USER](state) {
-        state.user = null;
+    [MUTATIONS.DELETE_SUBSCRIBER](state) {
+        state.subscriber = {
+            _id: '',
+            firstName: '',
+            lastName: '',
+            email: '',
+            roles: [],
+            subscribedCategories: [],
+            createdAt: '',
+            updatedAt: '',
+            analysis: {
+                totalCampaignClicks: 0,
+                totalNumberOfEmailSent: 0
+            }
+        };
     },
     [MUTATIONS.CHANGE_PAGE](state, page) {
         state.activePage = page;
-    }
+    },
 };
 
 export const actions = {
-    setUser({ commit }) {
-        commit(MUTATIONS.SET_USER);
+    async fetchSubscriber({ commit }) {
+        const subscriber = await this.$axios.$get('/subscriber/get');
+        commit(MUTATIONS.SET_SUBSCRIBER, subscriber.data);
     },
-    deleteUser({ commit }) {
-        commit(MUTATIONS.DELETE_USER);
+    deleteSubscriber({ commit }) {
+        commit(MUTATIONS.DELETE_SUBSCRIBER);
     },
     changePage({ commit }, page) {
         commit(MUTATIONS.CHANGE_PAGE, page);
-    }
+    },
 };
