76930a455ba8d6a694b1a9b63c2716081085d5ca
furkanogutcu <furkan.ogutcu@bil.omu.edu.tr>
2022-05-20 19:53:34 +0300

    Kampanya id ile email listesi alma ozelligi ekle

--- server/controllers/campaign.js
+++ server/controllers/campaign.js
@@ -1,9 +1,11 @@
 const Repository = require("./repository/repository");
 const CampaignService = require("../services/campaign");
+const SubscriberService = require("../services/subscriber");
 const ApiDataSuccess = require("../responses/success/apiDataSuccess");
 const httpStatus = require("http-status");
 const ApiError = require("../responses/error/apiError");
 const service = new CampaignService();
+const subscriberService = new SubscriberService();
 
 class Campaign extends Repository {
     increaseTotalClick = (req, res, next) => {
@@ -11,6 +13,32 @@ class Campaign extends Repository {
         service.increaseTotalClick(campaignId)
             .then((campaign) => {
                 return ApiDataSuccess.send(res, campaign, 'Campaign total click increased', httpStatus.OK);
+            })
+            .catch((error) => {
+                return next(new ApiError(error.message, error.code));
+            });
+    };
+
+    getEmailList = (req, res, next) => {
+        const campaignId = req.params.id;
+
+        //Kampanyayı getir
+        service.getById(campaignId)
+            .then((campaign) => {
+                //Kampanya kategorisine abone olan tüm aboneleri getir          
+                return subscriberService.getAllWithQuery({
+                    subscribedCategories: {
+                        $elemMatch: {
+                            category: campaign.category
+                        }
+                    }
+                });
+            })
+            .then((subscriberList) => {
+                const emailList = {
+                    emails: subscriberList.map(subscriber => subscriber.email)
+                };
+                return ApiDataSuccess.send(res, emailList, 'Campaign email list created successfully', httpStatus.OK);
             })
             .catch((error) => {
                 return next(new ApiError(error.message, error.code));
