76a0184ca5a4f317f72273b7631bdd03b18bfea4
furkanogutcu <furkan.ogutcu@bil.omu.edu.tr>
2022-05-21 12:02:37 +0300

    Hata sayfasi ekle

---
+++ client/layouts/error.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1 v-if="error.statusCode === 404">404 - Sayfa bulunamadı</h1>
+        <h1 v-else>Bir hata oluştu - {{ error.statusCode }} </h1>
+        <NuxtLink to="/dashboard">Panele geri dön</NuxtLink>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: 'error',
+    props: ['error']
+};
+</script>
